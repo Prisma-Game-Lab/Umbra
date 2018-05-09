94b052baeee1e54188dc5775748267e776ba946f
Hugo Tonette <hugo.tonette@gmail.com>
2018-05-08 21:09:18 -0300

    Basic Player Movement

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerControl : MonoBehaviour
+{
+    public float Gravity = 10;
+    public float Speed = 10;
+    public float Acceleration = 10;
+    public float JumpHeight = 10;
+
+    private float _currentSpeed;
+    private float _targetSpeed;
+    private Vector3 _amountToMove;
+
+    private PlayerPhysics _playerPhysics;
+
+    private void Start()
+    {
+        _playerPhysics = GetComponent<PlayerPhysics>();
+    }
+
+    private void Update()
+    {
+        _targetSpeed = Input.GetAxisRaw("Horizontal") * Speed;
+        _currentSpeed = IncrementTowards(_currentSpeed, _targetSpeed, Acceleration);
+        
+        if (_playerPhysics.IsGrounded)
+        {
+            _amountToMove.y = 0;
+
+            if (Input.GetButtonDown("Jump"))  // Jump
+            {
+                _amountToMove.y = JumpHeight;
+            }
+        }
+
+        _amountToMove.x = _currentSpeed;
+        _amountToMove.y -= Gravity * Time.deltaTime;
+        _playerPhysics.Move(_amountToMove * Time.deltaTime);
+    }
+
+    // Increase n towards target by a
+    private float IncrementTowards(float n, float target, float a)  // n = current speed; a = acceleration
+    {
+        if (n == target) return n;
+        else
+        {
+            float dir = Mathf.Sign(target - n);
+            n += a * Time.deltaTime * dir;
+            return (dir == Mathf.Sign(target - n)) ? n : target;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerPhysics.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour
+{
+    public LayerMask CollisionMask;
+
+    [HideInInspector] public bool IsGrounded;
+
+    private BoxCollider _colllider;
+    private Vector3 _colSize;
+    private Vector3 _colCenter;
+    private float skin = 0.005f;  // Space between the player and the ground, otherwise it can't detect the ground
+    private Ray _ray;
+    private RaycastHit _hit;
+
+    private void Start()
+    {
+        _colllider = GetComponent<BoxCollider>();
+        _colSize = _colllider.size;
+        _colCenter = _colllider.center;
+    }
+
+    public void Move(Vector2 moveAmount)
+    {
+        float deltaX = moveAmount.x;
+        float deltaY = moveAmount.y;
+        Vector2 pos = transform.position;
+
+        IsGrounded = false;
+        for (int i = 0; i < 3; i++)
+        {
+            float dir = Mathf.Sign(deltaY);
+            float x = (pos.x + _colCenter.x - _colSize.x / 2) + _colSize.x / 2 * i;
+            float y = pos.y + _colCenter.y + _colSize.y / 2 * dir;
+
+            _ray = new Ray(new Vector2(x, y), new Vector2(0, dir));
+            Debug.DrawRay(_ray.origin, _ray.direction, Color.red);
+
+            if (Physics.Raycast(_ray, out _hit, Mathf.Abs(deltaY), CollisionMask))
+            {
+                float dist = Vector3.Distance(_ray.origin, _hit.point);
+
+                if (dist > skin)
+                    deltaY = -dist + skin;
+                else
+                    deltaY = 0;
+                IsGrounded = true;
+                break;
+            }
+        }
+
+        Vector2 finalTransform = new Vector3(deltaX, deltaY);
+        
+        transform.Translate(finalTransform);
+    }
+}
