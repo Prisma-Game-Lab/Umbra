3d818e30c251388d9a94d39cade7f0d7c55e6d59
htonette <hugo.tonette@gmail.com>
2018-05-23 12:11:21 -0300

    Timer to die now works

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -7,12 +7,14 @@ public class PlayerHealth : MonoBehaviour
 {
     public int HealthMax = 1;
     [Range(0,60)] public float TimeToDie = 1;
+
     [HideInInspector] public int CurrentHealth;
 
     private string ThisSceneName;
 
     IEnumerator DeathTimer(float timer)
     {
+        this.gameObject.GetComponent<PlayerController>().enabled = false;
         yield return new WaitForSeconds(timer);
         PlayerDeath();
     }
@@ -27,8 +29,9 @@ public class PlayerHealth : MonoBehaviour
     {
         if(CurrentHealth <= 0)
         {
-            this.gameObject.GetComponent<PlayerController>().enabled = false;
-            DeathTimer(TimeToDie);
+            
+            //this.gameObject.GetComponent<PlayerController>().enabled = false;
+            StartCoroutine(DeathTimer(TimeToDie));
         }
     }
 
