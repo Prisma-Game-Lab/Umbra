d4b45a68c121db9e32fe9d8647ffb9c77c09fccc
htonette <hugo.tonette@gmail.com>
2018-06-15 12:25:35 -0300

    Started the Hook Chain Particle **not working yet**

---
+++ Assets/Scripts/Player/HookChain.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookChain : MonoBehaviour
+{
+    public ParticleSystem ChainParticle;
+    public GameObject Target;
+
+    private Vector3 _targetPos;
+
+    private void Start()
+    {
+        // _targetPos = Target.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        _targetPos = Target.transform.position;
+
+        // --- Put here the code to play the particle
+
+        // Rotates the particle
+        Vector3 diff = _targetPos - transform.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+        // --- Put here the code to stop the particle
+
+    }
+
+
+}
