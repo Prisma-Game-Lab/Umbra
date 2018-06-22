e55b14f151b3c14abc518f587992563cb54048cd
poncerapha <38406330+poncerapha@users.noreply.github.com>
2018-06-22 19:57:50 -0300

    aim test2

---
+++ Assets/Scripts/aim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aim : MonoBehaviour {
+
+
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+
+    void Start()
+    {
+        OnMouseEnter();
+        
+    }
+    void OnMouseEnter() 
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        
+    }
+
+}
