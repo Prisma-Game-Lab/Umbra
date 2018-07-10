b937bba7b8d530ab79da8cd2360e8e60f8c03d1e
Renan <renandafonte@gmail.com>
2018-07-10 11:51:31 -0300

    soundtransitionmanager agora pode mudar volume do som ambiente progressivo tb

---
+++ Assets/Scripts/Audio/AmbienceVolumeChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceVolumeChanger : MonoBehaviour {
+	public bool ShouldChangeVolume = false;
+	[Range(0,1)] public float NewVolume;
+
+	// Use this for initialization
+	void Start () {
+		if (ShouldChangeVolume) {
+			LevelManager.Instance.ProgressiveAmbientSound.volume = NewVolume;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Audio/BGMusicTransition.cs
+++ Assets/Scripts/Audio/BGMusicTransition.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BGMusicTransition : MonoBehaviour {
+	public bool ShouldChangeMusic = false;
 	[Tooltip("Música de fundo da fase")] public AudioClip BGMusic;
 	// Use this for initialization
 	void Start () {
-		if (LevelManager.Instance.BackgroundMusicSound.clip != BGMusic) {
+		if (ShouldChangeMusic && LevelManager.Instance.BackgroundMusicSound.clip != BGMusic) {
 			LevelManager.Instance.BackgroundMusicSound.Stop();
 			LevelManager.Instance.BackgroundMusicSound.clip = BGMusic;
 			LevelManager.Instance.BackgroundMusicSound.Play();
