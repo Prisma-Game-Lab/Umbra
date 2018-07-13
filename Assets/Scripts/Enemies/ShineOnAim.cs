408708cc4660d8a80fe1dbe4e9abacc54101b9d1
htonette <hugo.tonette@gmail.com>
2018-07-13 15:37:36 -0300

    Legolas shine when mouse hover

---
+++ Assets/Scripts/Enemies/ShineOnAim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShineOnAim : MonoBehaviour
+{
+    public Animator ShineAnim;
+    public SpriteRenderer ShineSprite;
+
+    private void OnMouseOver()
+    {
+        ShineSprite.enabled = true;
+        ShineAnim.SetBool("EnemyLight", true);
+    }
+
+    private void OnMouseExit()
+    {
+        ShineSprite.enabled = false;
+        ShineAnim.SetBool("EnemyLight", false);
+    }
+}
